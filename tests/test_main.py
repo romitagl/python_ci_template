b5fa79c19fd48e5fc466d1268a342296eff3b4a4
Gian Luigi Romita <romitagl@gmail.com>
2022-09-03 22:19:41 +0100

    Python 3.10 + Code Refactor

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,9 @@
-import pytest
-import sys
-# include src and folder (depending if running locally or in the container)
-sys.path.append("../src")
-sys.path.append("../app")
-# import hello_world function from the app.py file
-from app import hello_world
- 
-def test_hello_world():
-  assert hello_world() == "hello world"
+from src.app import hello_world, main
 
-if __name__ == "__main__":
-  # run pytest
-  pytest.main(sys.argv)
+
+def test_main():
+    main(None)
+
+
+def test_hello_world():
+    assert hello_world() == "hello world"
